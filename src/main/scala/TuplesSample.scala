c1e2b93538c26bc630cb46145d50adbf55e6eba6
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-02-25 16:37:57 +0100

    add tuple list methods

--- src/main/scala/TuplesSample.scala
+++ src/main/scala/TuplesSample.scala
@@ -1,3 +1,23 @@
-object TuplesSample {
-  //TODO
+object TuplesSample extends App{
+  //shapeless allows standard Scala tuples to be manipulated in exactly the same ways as HLists
+
+  import shapeless.syntax.std.tuple._
+
+  (23, "foo", true).head
+  (23, "foo", true).tail
+  (23, "foo", true).drop(2)
+  (23, "foo", true).take(2)
+  (23, "foo", true).split(1)
+  23 +: ("foo", true)
+  (23, "foo") :+ true
+
+  import shapeless.poly._
+  import shapeless.Id
+
+  object option extends (Id ~> Option) {
+    def apply[T](t: T) = Option(t)
+  }
+  (23, "foo", true) map option
+
+  (23, "foo", true).productElements
 }
