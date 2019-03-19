e7ea51b7c5ada169f6a17b4807fb0bd54bd0d0cb
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-03-19 16:23:42 +0100

    delete junior

--- src/main/scala/CoproductSample.scala
+++ src/main/scala/CoproductSample.scala
@@ -2,31 +2,31 @@ import shapeless._
 
 object CoproductSample {
 
-  case class Lead(name: String, alias: String, mastery: String)
+  case class Lead(name: String, alias: String, area: String)
   case class Senior(name: String, mastery: List[String], years: Int)
-  case class Junior(name: String, daysWithoutBreakMaster: Int)
+  case class Engineer(name: String, haskellian: Boolean)
 
   //Std
 
   sealed trait InheritanceDeveloper
 
-  type optDeveloper = (Option[Lead], Option[Senior], Option[Junior])
+  type optDeveloper = (Option[Lead], Option[Senior], Option[Engineer])
 
-  type eitherDeveloper = Either[Lead, Either[Senior, Junior]]
+  type eitherDeveloper = Either[Lead, Either[Senior, Engineer]]
 
   //Shapeless
 
-  type Developer = Lead :+: Senior :+: Junior :+: CNil
+  type Developer = Lead :+: Senior :+: Engineer :+: CNil
 
   val lead   = Lead("Juan Pedro", "who is worth it", "Everything")
   val senior = Senior("pepegar", List("Alien", "FP"), 7)
-  val junior = Junior("yawolf", 31)
+  val engineer = Engineer("yawolf", true)
 
   val leadDev   = Coproduct[Developer](lead)
   val seniorDev = Coproduct[Developer](senior)
-  val juniorDev = Coproduct[Developer](junior)
+  val engineerDev = Coproduct[Developer](engineer)
 
-  type PlainDeveloper = Senior :+: Junior :+: CNil
+  type PlainDeveloper = Senior :+: Engineer :+: CNil
 
   leadDev.eliminate(lead => Coproduct[PlainDeveloper](Senior(lead.name, List("FP"), 14)), identity) // Plain Developer
 
@@ -37,12 +37,12 @@ object CoproductSample {
   leadDev.rotateLeft(1)  // Senior :+: Junior :+: Lead :+: CNil
   leadDev.rotateRight(1) // Junior :+: Lead :+: Senior :+: CNil
 
-  val devList: List[Developer] = List(leadDev, seniorDev, juniorDev)
+  val devList: List[Developer] = List(leadDev, seniorDev, engineerDev)
 
   object calculateDevPoints extends Poly1 {
     implicit def caseLead: Case.Aux[Lead, Int]     = at[Lead](_ => 5)
     implicit def caseSenior: Case.Aux[Senior, Int] = at[Senior](_.mastery.size + 1)
-    implicit def caseJunior: Case.Aux[Junior, Int] = at[Junior](jn => if (jn.daysWithoutBreakMaster >= 31) 2 else 1)
+    implicit def caseJunior: Case.Aux[Engineer, Int] = at[Engineer](jn => if (jn.haskellian) 1 else 2)
   }
 
   devList.map(_.map(calculateDevPoints).unify).sum
