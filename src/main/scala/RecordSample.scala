3534ead1efb2d1e8528ffcd6a3d4a97956773556
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-03-19 14:55:50 +0100

    more std samples

--- src/main/scala/RecordSample.scala
+++ src/main/scala/RecordSample.scala
@@ -4,12 +4,18 @@ import shapeless.syntax.singleton._
 
 object RecordSample {
 
-  val book =
-    ("author" ->> "Benjamin Pierce") ::
-      ("title" ->> "Types and Programming Languages") ::
-      ("id" ->> 262162091) ::
-      ("price" ->> 44.11) ::
-      HNil
+  //Std
+
+  val strFieldsBook = Map("author" -> "Benjamin Pierce", "title" -> "Types and Programming Languages")
+
+  val numFieldsBook = Map(("id" -> 262162091, "price" -> 44.11))
+
+  val bookSection = Map(101 -> "History")
+
+  //Shapeless
+
+  val book = ("author" ->> "Benjamin Pierce") :: (101 ->> "History") ::
+    ("id" ->> 262162091) :: ("price" ->> 44.11) :: HNil
 
   //Get values
   book("author") //Benjamin Pierce
